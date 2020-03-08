680472aca8b08688d0f0354e094b8f01da55f47a
Swiderski <bsswider@amazon.com>
2020-03-08 13:38:29 +0000

    Updated, working

--- src/app/module/shared/actor-card/actor-card.component.ts
+++ src/app/module/shared/actor-card/actor-card.component.ts
@@ -3,7 +3,7 @@ import { Component,Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-actor-card',
   templateUrl: './actor-card.component.html',
-  styleUrls: ['./actor-card.component.css']
+  styleUrls: ['./actor-card.component.scss']
 })
 export class ActorCardComponent implements OnInit {
 
