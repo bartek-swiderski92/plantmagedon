fd106faf8019a22318b6e938e5fe803b1209ef49
Swiderski <bsswider@amazon.com>
2020-03-09 17:26:02 +0000

    Minor fixes

--- src/app/module/main/main.module.ts
+++ src/app/module/main/main.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActorCardComponent } from '../shared/actor-card/actor-card.component';
 import { MainRoutingModule } from './main-routing.module';
-
+import { ButtonComponent } from '../shared/button/button.component';
 import { MainComponent } from './main.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    ActorCardComponent
+    ActorCardComponent,
+    ButtonComponent
   ],
+
   imports: [
     CommonModule,
     MainRoutingModule
